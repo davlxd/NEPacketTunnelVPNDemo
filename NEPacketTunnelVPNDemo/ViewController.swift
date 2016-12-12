2ca3243b3fa9756147fde979a2042f1733c84a47
lxd <i@lxd.me>
2016-12-12 11:35:41 +0800

    Add UI stuff

--- NEPacketTunnelVPNDemo/ViewController.swift
+++ NEPacketTunnelVPNDemo/ViewController.swift
@@ -7,17 +7,122 @@
 //
 
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
+    var vpnManager: NETunnelProviderManager = NETunnelProviderManager()
+    @IBOutlet var connectButton: UIButton!
+
+    // Hard code VPN configurations
+    let tunnelBundleId = "ne.packet.tunnel.vpn.demo.tunnel"
+    let serverAddress = "<server-ip>"
+    let serverPort = "54345"
+    let mtu = "1400"
+    let ip = "10.8.0.2"
+    let subnet = "255.255.255.0"
+
+
+    private func initVPNTunnelProviderManager() {
+        NETunnelProviderManager.loadAllFromPreferences { (savedManagers: [NETunnelProviderManager]?, error: Error?) in
+            if let error = error {
+                print(error)
+            }
+            if let savedManagers = savedManagers {
+                if savedManagers.count > 0 {
+                    self.vpnManager = savedManagers[0]
+                }
+            }
+
+            self.vpnManager.loadFromPreferences(completionHandler: { (error:Error?) in
+                if let error = error {
+                    print(error)
+                }
+
+                let providerProtocol = NETunnelProviderProtocol()
+                providerProtocol.providerBundleIdentifier = self.tunnelBundleId
+
+                providerProtocol.providerConfiguration = ["port": self.serverPort,
+                                                          "server": self.serverAddress,
+                                                          "ip": self.ip,
+                                                          "subnet": self.subnet,
+                                                          "mtu": self.mtu
+                ]
+                providerProtocol.serverAddress = self.serverAddress
+                self.vpnManager.protocolConfiguration = providerProtocol
+                self.vpnManager.localizedDescription = "NEPacketTunnelVPNDemoConfig"
+                self.vpnManager.isEnabled = true
+
+                self.vpnManager.saveToPreferences(completionHandler: { (error:Error?) in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("Save successfully")
+                    }
+                })
+                self.VPNStatusDidChange(nil)
+
+            })
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
+        initVPNTunnelProviderManager()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.VPNStatusDidChange(_:)), name: NSNotification.Name.NEVPNStatusDidChange, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func VPNStatusDidChange(_ notification: Notification?) {
+        print("VPN Status changed:")
+        let status = self.vpnManager.connection.status
+        switch status {
+        case .connecting:
+            print("Connecting...")
+            connectButton.setTitle("Disconnect", for: .normal)
+            break
+        case .connected:
+            print("Connected...")
+            connectButton.setTitle("Disconnect", for: .normal)
+            break
+        case .disconnecting:
+            print("Disconnecting...")
+            break
+        case .disconnected:
+            print("Disconnected...")
+            connectButton.setTitle("Connect", for: .normal)
+            break
+        case .invalid:
+            print("Invliad")
+            break
+        case .reasserting:
+            print("Reasserting...")
+            break
+        }
+    }
+
+    @IBAction func go(_ sender: UIButton, forEvent event: UIEvent) {
+        print("Go!")
+
+        self.vpnManager.loadFromPreferences { (error:Error?) in
+            if let error = error {
+                print(error)
+            }
+            if (sender.title(for: .normal) == "Connect") {
+                do {
+                    try self.vpnManager.connection.startVPNTunnel()
+                } catch {
+                    print(error)
+                }
+            } else {
+                self.vpnManager.connection.stopVPNTunnel()
+            }
+        }
     }
 
 
