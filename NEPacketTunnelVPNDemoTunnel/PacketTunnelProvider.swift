5d2b057797dacdc183716acdfa502e4a3a94c39a
lxd <i@lxd.me>
2017-02-15 01:08:17 +0800

    Complete NE extension code

--- NEPacketTunnelVPNDemoTunnel/PacketTunnelProvider.swift
+++ NEPacketTunnelVPNDemoTunnel/PacketTunnelProvider.swift
@@ -9,24 +9,100 @@
 import NetworkExtension
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
+    var session: NWUDPSession? = nil
+    var conf = [String: AnyObject]()
 
-    override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
-        //code
+    // These 2 are core methods for VPN tunnelling
+    //   - read from tun device, encrypt, write to UDP fd
+    //   - read from UDP fd, decrypt, write to tun device
+    func tunToUDP() {
+        self.packetFlow.readPackets { (packets: [Data], protocols: [NSNumber]) in
+            for packet in packets {
+                // This is where encrypt() should reside
+                // A comprehensive encryption is not easy and not the point for this demo
+                // I just omit it
+                self.session?.writeDatagram(packet, completionHandler: { (error: Error?) in
+                    if let error = error {
+                        print(error)
+                        self.setupUDPSession()
+                        return
+                    }
+                })
+            }
+            // Recursive to keep reading
+            self.tunToUDP()
+        }
+    }
+
+    func udpToTun() {
+        // It's callback here
+        session?.setReadHandler({ (_packets: [Data]?, error: Error?) -> Void in
+            if let packets = _packets {
+                // This is where decrypt() should reside, I just omit it like above
+                self.packetFlow.writePackets(packets, withProtocols: [NSNumber](repeating: AF_INET as NSNumber, count: packets.count))
+            }
+        }, maxDatagrams: NSIntegerMax)
+    }
+
+    func setupPacketTunnelNetworkSettings() {
+        let tunnelNetworkSettings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: self.protocolConfiguration.serverAddress!)
+        tunnelNetworkSettings.iPv4Settings = NEIPv4Settings(addresses: [conf["ip"] as! String], subnetMasks: [conf["subnet"] as! String])
+
+        // Refers to NEIPv4Settings#includedRoutes or NEIPv4Settings#excludedRoutes,
+        // which can be used as basic whitelist/blacklist routing.
+        // This is default routing.
+        tunnelNetworkSettings.iPv4Settings?.includedRoutes = [NEIPv4Route.default()]
+
+        tunnelNetworkSettings.mtu = Int(conf["mtu"] as! String) as NSNumber?
+
+        let dnsSettings = NEDNSSettings(servers: (conf["dns"] as! String).components(separatedBy: ","))
+        // This overrides system DNS settings
+        dnsSettings.matchDomains = [""]
+        tunnelNetworkSettings.dnsSettings = dnsSettings
+
+        self.setTunnelNetworkSettings(tunnelNetworkSettings) { (error: Error?) -> Void in
+            self.udpToTun()
+        }
+    }
+
+    func setupUDPSession() {
+        if self.session != nil {
+            self.reasserting = true
+            self.session = nil
+        }
+        let serverAddress = self.conf["server"] as! String
+        let serverPort = self.conf["port"] as! String
+        self.reasserting = false
+        self.setTunnelNetworkSettings(nil) { (error: Error?) -> Void in
+            if let error = error {
+                print(error)
+            }
+            self.session = self.createUDPSession(to: NWHostEndpoint(hostname: serverAddress, port: serverPort), from: nil)
+            self.setupPacketTunnelNetworkSettings()
+        }
+    }
+
+    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        conf = (self.protocolConfiguration as! NETunnelProviderProtocol).providerConfiguration! as [String : AnyObject]
+        self.setupUDPSession()
+        self.tunToUDP()
     }
 
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        //code
+        session?.cancel()
+        super.stopTunnel(with: reason, completionHandler: completionHandler)
     }
 
     override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)? = nil) {
-        //code
+        if let handler = completionHandler {
+            handler(messageData)
+        }
     }
 
     override func sleep(completionHandler: @escaping () -> Void) {
-        //code
+        completionHandler()
     }
 
 	override func wake() {
-		// Add code here to wake up.
 	}
 }
